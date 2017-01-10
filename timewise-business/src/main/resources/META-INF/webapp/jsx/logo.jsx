3a90e9f770d8a0a16668499f1851afc289e14cdb
Erik H <erikhakan@gmail.com>
2017-01-10 22:50:32 +0100

    Started on topbar

--- timewise-business/src/main/resources/META-INF/webapp/jsx/logo.jsx
+++ timewise-business/src/main/resources/META-INF/webapp/jsx/logo.jsx
@@ -2,13 +2,9 @@ import React from "react";
 
 export default class Logo extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.props = props;
-    // }
     render() {
         return <div className={this.props.size ? this.props.size + " logo" : "logo"}>
-                <img src="http://lorempixel.com/100/50/" width={100} height={50} />
+                <img src="/img/logo_withtext_100px.png" width="auto" height={50} />
             </div>
     }
 }
